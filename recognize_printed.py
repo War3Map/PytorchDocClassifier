d6898617165474bdedb5f0819b4bcf5df192b05f
ivan.morzhov <ivan.morzhov@fogstream.ru>
2021-10-25 18:48:11 +1000

    Correct recognizer. Temporary delete image angle correction.

--- recognize_printed.py
+++ recognize_printed.py
@@ -9,6 +9,12 @@ TESSERACT_PATH = r"D:\Tesseract\tesseract.exe"
 pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
 
 
+def show_image(img):
+    cv2.imshow("image", img)
+    cv2.waitKey(0)  # waits until a key is pressed
+    cv2.destroyAllWindows()  # destroys the window showing image
+
+
 def prepare_image(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.medianBlur(img, 3)
@@ -16,6 +22,8 @@ def prepare_image(img):
     rgb_planes = cv2.split(img)
     result_planes = []
     result_norm_planes = []
+
+    show_image(img)
     for plane in rgb_planes:
         dilated_img = cv2.dilate(plane, np.ones((7, 7), np.uint8))
         bg_img = cv2.medianBlur(dilated_img, 21)
@@ -24,21 +32,33 @@ def prepare_image(img):
     img = cv2.merge(result_planes)
     kernel = np.ones((2, 2), np.uint8)
     img = cv2.dilate(img, kernel, iterations=1)
+    show_image(img)
     img = cv2.erode(img, kernel, iterations=2)
+    show_image(img)
     skel = img.copy()
     kernel = np.ones((3, 3), np.uint8)
     erod = cv2.morphologyEx(img, cv2.MORPH_ERODE, kernel)
     temp = cv2.morphologyEx(erod, cv2.MORPH_DILATE, kernel)
     temp = cv2.subtract(img, temp)
     skel = cv2.bitwise_or(skel, temp)
+    show_image(img)
     result_image = cv2.threshold(skel, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
 
     return result_image
 
 
 # проверка угла изображения, выравнивание
 def correct_rotation(imag):
+    """
+    Corrects rotation angle for text image
+    :param imag:
+    :return:
+    """
     img_before = imag
+
+    grayscale = cv2.cvtColor(img_before, cv2.COLOR_BGR2GRAY)
 
     img_gray = cv2.cvtColor(img_before, cv2.COLOR_BGR2GRAY)
     img_edges = cv2.Canny(img_gray, 100, 100, apertureSize=3)
@@ -65,7 +85,11 @@ def tesseract_recognize(image_path):
     :return:
     """
     img = cv2.imread(image_path)
-    img = correct_rotation(prepare_image(img))
+
+    # TODO: Do rotation correction
+    # img = correct_rotation(prepare_image(img))
+
+
     # Adding custom options
     custom_config = r'--oem 3 --psm 6'
     result = pytesseract.image_to_string(img, config=custom_config, lang='rus')
@@ -86,3 +110,10 @@ def save_to_txt(string_data, output_filename):
 
 def recognize_image(source_path, result_path):
     save_to_txt(tesseract_recognize(source_path), result_path)
+
+
+TEST_IMAGE = r"C:\Users\IVAN\Desktop\Texts\test\printed\doc21.png"
+
+
+if __name__ == "__main__":
+    recognize_image(TEST_IMAGE, "test_result")
